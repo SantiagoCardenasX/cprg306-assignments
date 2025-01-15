f8fe19f765c18c73175aa8ea455b38bd6f31202f
SantiagoCardenasX <santiago.cardenasdelgadillo@edu.sait.ca>
2025-01-15 15:34:56 -0700

    All done

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Santiago Cardenas</h1>
+      <p><Link href={"https://github.com/SantiagoCardenasX"}>Click for Github Profile</Link></p>
+    </main>
+  );
+}
